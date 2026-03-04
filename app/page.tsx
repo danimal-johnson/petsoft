5df7d1a2e9cb3dbace9e1719748730c05fb60bbf
Dan Johnson <danimal.johnson@gmail.com>
2026-03-03 17:02:13 -0800

    Initial project commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1 className="text-5xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-7xl">
+          PetSoft
+        </h1>
+        <p className="mt-5 text-2xl text-gray-500 dark:text-gray-400">
+          Find a pet sitter near you.
+        </p>
+      </main>
+    </div>
+  );
+}
