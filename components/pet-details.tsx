8eb864a26bf95484e375d4687b6873d2345edf43
Dan Johnson <danimal.johnson@gmail.com>
2026-03-21 00:08:46 -0700

    Fix types for release build

--- components/pet-details.tsx
+++ components/pet-details.tsx
@@ -4,8 +4,7 @@ import { usePetContext } from "@/lib/hooks";
 import Image from "next/image";
 import { Pet } from "@/lib/types";
 import PetButton from "./pet-button";
-import { deletePet } from "@/actions/actions";
-import { startTransition, useTransition } from "react";
+import { useTransition } from "react";
 
 export default function PetDetails() {
   const { selectedPet } = usePetContext();
@@ -31,12 +30,12 @@ function TopBar({ pet }: { pet: Pet }) {
   return (
     <div className="flex items-center bg-white px-8 py-5 border-b border-light">
       <Image
-        src={pet?.imageUrl || "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png" }
+        src={pet.imageUrl || "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png" }
         alt="Pet Image"
         width={75} height={75}
         className="h-[75px] w-[75px] object-cover rounded-full"
       />
-      <h2 className="text-3xl font-semibold leading-7 ml-5">{pet?.name}</h2>
+      <h2 className="text-3xl font-semibold leading-7 ml-5">{pet.name}</h2>
 
       <div className="ml-auto flex items-center gap-3">
         <PetButton actionType="edit">Edit</PetButton>
@@ -71,11 +70,11 @@ function Details({ pet }: { pet: Pet }) {
   );
 }
 
-function Notes({ pet }: { pet?: Pet }) {
+function Notes({ pet }: { pet: Pet }) {
   return (
     <section className="flex-1 bg-white px-7 py-5 rounded-md mb-9 mx-8 border border-light">
       <h3 className="text-[13px] font-medium uppercase text-zinc-700">Notes</h3>
-      <p className="mt-1 text-sm text-gray-500">{pet?.notes}</p>
+      <p className="mt-1 text-sm text-gray-500">{pet.notes}</p>
     </section>
   );
 }
